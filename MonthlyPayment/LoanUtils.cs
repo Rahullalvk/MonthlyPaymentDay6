d62da6384b6a7c641f69d1b8ddaaebced92714af
Rahulalvk <rahulal432@gmail.com>
2023-05-04 04:42:26 -0700

    programe to find the compound interest and to find the monthtly loan amount should pay

---
+++ MonthlyPayment/LoanUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthlyPayment
+{
+    internal class LoanUtils
+    {
+            public static double monthlyPayment(double P, int Y, double R)
+            {
+                int n = 12;  
+                int t = Y * n;  
+                double r = R / (n * 100);  
+                double A = P * Math.Pow(1 + r, t);
+                double monthlyPayment = A * r / (1 - Math.Pow(1 + r, -t));
+
+                return monthlyPayment;
+            }
+    }
+}
---
+++ MonthlyPayment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthlyPayment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter principal amount: ");
+            double P = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of years: ");
+            int Y = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter interest rate: ");
+            double R = double.Parse(Console.ReadLine());
+
+            double monthlyPayment = LoanUtils.monthlyPayment(P, Y, R);
+
+            Console.WriteLine($"Monthly Payment: {monthlyPayment}");
+        }
+    }
+}
